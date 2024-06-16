2df6c0dc0faec3898ace58826ede7395754f4f8e
Gopi <GopiR5@yahoo.com>
2024-06-16 21:06:08 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <section className="pricing py-5  ">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+            <hr/>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Single User</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ 5GB Storage</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Public Projects</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Community Access</div>
+              <div className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Χ Unlimited Private Projects</div>
+              <div className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Χ Dedicated Phone Support</div>
+              <div className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Χ Free Subdomain</div>
+              <div className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Χ Monthly Status Reports</div>
+            
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div className="col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
+            <hr/>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ 5 Users</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ 50GB Storage</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Public Projects</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Community Access</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Private Projects</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Dedicated Phone Support</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Free Subdomain</div>
+              <div className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Χ Monthly Status Reports</div>
+             
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
+            <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
+            <hr/>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Users</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ 150GB Storage</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Public Projects</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Community Access</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Unlimited Private Projects</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Dedicated Phone Support</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Free Subdomain</div>
+              <div><span className="fa-li"><i className="fas fa-check"></i></span>✓ Monthly Status Reports</div>
+           
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  );
+}
+
+export default App;
